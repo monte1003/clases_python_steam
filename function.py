055516b458bae9c9c7e2ef5a4edbe49c830e8316
monte1003 <isaacmonterrosaguzman@gmail.com>
2023-02-07 19:32:23 -0500

    Buenas Practicas

---
+++ function.py
@@ -0,0 +1,19 @@
+def area_rectangulo(
+    x:float,
+    y:float
+) -> float:
+    """
+    Función que calcula el área de un rectangulo
+    ---params---
+    - x(float): Base del Rectángulo
+    - y(float): Altura del Rectángulo 
+    
+    ---return---
+    area (float): Area de mi rectangulo
+    """
+    area = x * y
+    return area
+base: float = float(input('Introduce la base del rectangulo: '))
+altura: float = float(input('Introduce la altura del rectángulo: '))
+result = area_rectangulo(base,altura)
+print(f'El área del rectángulo es: {result}')    
