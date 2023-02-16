9bc8185daa58fa2eca695283853e1524288bb1a3
monte1003 <isaacmonterrosaguzman@gmail.com>
2023-02-15 22:38:39 -0500

    Mini-Proyecto Caracteres Frase

---
+++ ejercicios_funciones/ejercicios_caracteres/funcion_caracteres.py
@@ -0,0 +1,16 @@
+def palabra(
+    p: str
+) -> list:
+    """_summary_
+
+    Args:
+        p (str): _description_
+
+    Returns:
+        list: _description_
+    """
+    p = p.split(" ")
+    caracteres = []
+    for i in p:
+        caracteres.append(i)
+    return caracteres
